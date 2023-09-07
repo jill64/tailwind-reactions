9b9bc5a33a476b751d13be765f1bbe7a1331ec89
jill64 <intents.turrets0h@icloud.com>
2023-09-07 14:11:44 +0900

    add preview src

---
+++ packages/site/src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<h1 class="text-4xl font-bold m-4">tailwind-reactions</h1>
+<button class="push-effect bg-blue-500 text-white px-4 py-2 rounded-full">
+  push-effect
+</button>
+<button class="pop-effect bg-blue-500 text-white px-4 py-2 rounded-full">
+  pop-effect
+</button>
+<div class="m-4">
+  <div class="inline-grid grid-cols-2 items-center gap-4">
+    <input
+      placeholder="focus-under"
+      class="focus-under bg-inherit text-inherit border-zinc-400 focus:border-blue-600"
+    />
+    <input
+      placeholder="focus-under-bold"
+      class="focus-under-bold bg-inherit text-inherit border-zinc-400 focus:border-blue-600"
+    />
+    <input
+      placeholder="focus-under-2"
+      class=" focus-under-2 bg-inherit text-inherit border-zinc-400 focus:border-blue-600"
+    />
+    <input
+      placeholder="focus-under-bold-2"
+      class="focus-under-bold-2 bg-inherit text-inherit border-zinc-400 focus:border-blue-600"
+    />
+    <input
+      placeholder="focus-under-3"
+      class=" focus-under-3 bg-inherit text-inherit border-zinc-400 focus:border-blue-600"
+    />
+    <input
+      placeholder="focus-under-bold-3"
+      class="focus-under-bold-3 bg-inherit text-inherit border-zinc-400 focus:border-blue-600"
+    />
+  </div>
+</div>
